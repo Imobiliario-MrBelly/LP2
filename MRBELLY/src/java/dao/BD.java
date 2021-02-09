562f8eb28d6ef4065bf3fa12021519b91de4245f
Rennan <rennandamiao@gmail.com>
2021-02-09 11:00:33 -0300

    Create BD.java

---
+++ MRBELLY/src/java/dao/BD.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import jdk.jfr.internal.tool.Main;
+
+public class BD {
+
+    private static BD instancia = new BD();
+
+    public BD getInstancia() {
+        return instancia;
+    }
+
+    public void getConexao() throws SQLException {
+
+        try (Connection conexao = DriverManager.getConnection("jdbc:mysql:sql5.freesqldatabase.com", "sql5391594", "TtMDfIzTer")) {
+            System.out.println("Conectado!");
+        }
+    }
+    public static void main(String [] args) throws SQLException{ 
+    
+   BD a=new BD();
+   a.getConexao();
+    }
+}
