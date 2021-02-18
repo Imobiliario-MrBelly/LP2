3bbf7046342f22776232c6ffe5a2ef8d672be7f0
Rennan <rennandamiao@gmail.com>
2021-02-18 15:57:22 -0300

    Update Garagem.java

--- MRBELLY/src/java/models/Garagem.java
+++ MRBELLY/src/java/models/Garagem.java
@@ -1,15 +1,23 @@
 package models;
 
 public class Garagem extends Imovel{
+    int id;
     private int tamanho;
     private boolean coberturaChuva;
     private String localizacao;
 
-    public Garagem(int tamanho, boolean coberturaChuva, String localizacao, Endereco endereco, double area, String descricao, double condominio, double iptu) {
-        super(endereco, area, descricao, condominio, iptu);
+    public Garagem(int tamanho, boolean coberturaChuva, String localizacao, Endereco endereco, double area, String descricao, double condominio, double iptu,Locador locador) {
+        super(endereco, area, descricao, condominio, iptu, locador);
+     
         this.tamanho = tamanho;
         this.coberturaChuva = coberturaChuva;
         this.localizacao = localizacao;
+    }
+public int getId(){
+    return id;
+}
+    public boolean isCoberturaChuva() {
+        return coberturaChuva;
     }
 
     public String getLocalizacao() {
