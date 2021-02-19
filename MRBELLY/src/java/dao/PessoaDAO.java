171b3be823f40fba9bfe9d7f7c2db9a8cd9ad903
junio-goulart <55002214+junio-goulart@users.noreply.github.com>
2021-02-19 20:10:24 -0300

    Modificação Pessoa

--- MRBELLY/src/java/dao/PessoaDAO.java
+++ MRBELLY/src/java/dao/PessoaDAO.java
@@ -1,14 +1,34 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import models.Pessoa;
 
-public class PessoaDAO {
+public class PessoaDAO extends DAO {
+    
     static private PessoaDAO instancia = new PessoaDAO();
+    
     static PessoaDAO getInstancia(){
         return instancia;
     }
-    public Pessoa obterPessoa(int id){
-        return null;
+    
+    public void gravar(Pessoa pessoa) throws SQLException,ClassNotFoundException, CloneNotSupportedException{
+        Connection conexao = null;
+        PreparedStatement comando = null;
+        
+        try {
+            conexao = BD.getInstancia().getConexao();
+            
+            comando  = conexao.prepareStatement("INSERT INTO PESSOA(nome, dataCadastro, flag");
+            comando.setString(1, pessoa.);
+        } finally{
+            
+        }
     }
+    
+    
 
 }
