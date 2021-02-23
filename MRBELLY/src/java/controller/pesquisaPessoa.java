90fe98e10936f3da808ee6bc82cb1c592193c1a4
Rennan <rennandamiao@gmail.com>
2021-02-23 14:58:31 -0300

    Update pesquisaPessoa.java

--- MRBELLY/src/java/controller/pesquisaPessoa.java
+++ MRBELLY/src/java/controller/pesquisaPessoa.java
@@ -7,10 +7,13 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Pessoa;
 
 /**
  *
@@ -30,18 +33,15 @@ public class pesquisaPessoa extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet pesquisaCliente</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet pesquisaCliente at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try {
+            request.setAttribute("pessoas", Pessoa.obterPessoas());
+            RequestDispatcher view = request.getRequestDispatcher("/pesquisaPessoa.jsp");
+            view.forward(request, response);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new ServletException(e);
+
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
