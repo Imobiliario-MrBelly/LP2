bd13af1bf16717a203d9805b1aa03821a1b0b335
Rennan <rennandamiao@gmail.com>
2021-03-20 23:36:41 -0300

    Update ManterLocador.java

--- MRBELLY/src/java/controller/ManterLocador.java
+++ MRBELLY/src/java/controller/ManterLocador.java
@@ -117,12 +117,12 @@ public class ManterLocador extends HttpServlet {
                             try {
                                 login.editar();
                             } catch (Exception e) {
-                                enderecoAntigo.editar();
-                                throw new Exception(e);
+//                                enderecoAntigo.editar();
+//                                throw new Exception(e);
                             }
                         } catch (Exception e) {
-                            pessoaAntiga.editar();
-                            throw new Exception(e);
+//                            pessoaAntiga.editar();
+//                            throw new Exception(e);
                         }
                     } catch (Exception e) {
 
@@ -134,10 +134,10 @@ public class ManterLocador extends HttpServlet {
                         login.excluir();
                         locador.excluir();
                     }
-                }
+                }}
                 RequestDispatcher view = request.getRequestDispatcher("pesquisaLocador");
                 view.forward(request, response);
-            }
+            
         } catch (IOException e) {
             throw new ServletException(e);
         } catch (ServletException e) {
